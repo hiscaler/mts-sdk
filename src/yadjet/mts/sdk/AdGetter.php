664f790fb8c1ea633c6ced5dea383aae14d1112b
hiscaler <hiscaler@gmail.com>
2016-05-12 23:45:26 +0800

    添加广告数据拉取

---
+++ src/yadjet/mts/sdk/AdGetter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace yadjet\mts\sdk;
+
+use Yii;
+
+/**
+ * 广告数据拉取
+ * 
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class AdGetter extends DataGetter
+{
+
+    const TYPE_PICTURE = 0;
+    const TYPE_FLASH = 1;
+    const TYPE_TEXT = 2;
+
+    public static function rows($alias)
+    {
+        
+    }
+
+    public static function one($alias)
+    {
+        $db = Yii::$app->getDb();
+        $space = $db->createCommand('SELECT [[id]], [[width]], [[height]] FROM {{%ad_space}} WHERE [[tenant_id]] = :tenantId AND [[status]] = :status AND [[enabled]] = :enabled AND [[alias]] = :alias')->bindValues([
+                ':tenantId' => self::getConstantValue('TENANT_ID'),
+                ':status' => self::BOOLEAN_TRUE,
+                ':enabled' => self::BOOLEAN_TRUE,
+                ':alias' => strtolower(trim($alias))
+            ])->queryOne();
+        if ($space === false) {
+            return null;
+        }
+        $ad = $db->createCommand('SELECT [[id]], [[type]], [[url]], [[file_path]], [[text]], [[end_datetime]], [[message]] FROM {{%ad}} WHERE [[space_id]] = :spaceId AND [[status]] = :status AND [[enabled]] = :enabled AND [[begin_datetime]] <= :now ORDER BY [[end_datetime]] DESC')->bindValues([
+                ':spaceId' => $space['id'],
+                ':status' => self::BOOLEAN_TRUE,
+                ':enabled' => self::BOOLEAN_TRUE,
+                ':now' => time()
+            ])->queryOne();
+        if ($ad === false) {
+            return null;
+        } else {
+            if ($ad['end_datetime'] < time()) {
+                $output = $ad['message'];
+            } else {
+                $db->createCommand('UPDATE {{%ad}} SET [[hits_count]] = [[hits_count]] + 1, [[views_count]] = [[views_count]] + 1 WHERE [[id]] = :id')->bindValue(':id', $ad['id'], PDO::PARAM_INT)->execute();
+                switch ($ad['type']) {
+                    case static::TYPE_PICTURE:
+                        $output = '<img src="' . $ad['file_path'] . '" width="' . $space['width'] . '" height="' . $space['height'] . '">';
+                        break;
+                    case static::TYPE_TEXT:
+                        $output = $ad['text'];
+                        break;
+                    default:
+                        $output = null;
+                        break;
+                }
+                if (!empty($ad['url'])) {
+                    $output = '<a href="' . $ad['url'] . '" target="_balnk">' . $output . '</a>';
+                }
+            }
+
+            return $output;
+        }
+    }
+
+}
