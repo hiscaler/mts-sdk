bd2bf81075a3eac67df9f46a7123cb15dc64053b
hiscaler <hiscaler@gmail.com>
2016-05-12 23:45:42 +0800

    添加幻灯片数据拉取类

---
+++ src/yadjet/mts/sdk/SlideGetter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace yadjet\mts\sdk;
+
+use Yii;
+
+/**
+ * 幻灯片数据拉取
+ * 
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class SlideGetter extends DataGetter implements DataGetterInterface
+{
+
+    public static function parseQuery($fields = null, $where = array(), $orderBy = 'ordering.asc', $offset = 0, $limit = 10)
+    {
+        if (empty($fields)) {
+            $fields = 'title,url,url_open_target,description';
+        }
+
+        $condition = [
+            'tenant_id' => self::getConstantValue('TENANT_ID')
+        ];
+        if ($where) {
+            if (isset($where['group'])) {
+                $condition['group_id'] = strpos($where['group'], ',') === false ? (int) $where['group'] : explode(',', $where['group']);
+            }
+        }
+
+        return (new \yii\db\Query())
+                ->select($fields)
+                ->from('{{%slide}}')
+                ->where($condition)
+                ->orderBy(self::parseOrderBy($orderBy))
+                ->offset($offset)
+                ->limit($limit);
+    }
+
+}
