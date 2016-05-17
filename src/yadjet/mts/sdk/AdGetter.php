d7b6d6f0ebe19a4f81ed8eea7511eb61e61cc6f8
hiscaler <hiscaler@gmail.com>
2016-05-17 23:05:56 +0800

    支持获取广告列表

--- src/yadjet/mts/sdk/AdGetter.php
+++ src/yadjet/mts/sdk/AdGetter.php
@@ -18,7 +18,52 @@ class AdGetter extends DataGetter
 
     public static function rows($alias)
     {
-        
+        $db = Yii::$app->getDb();
+        $space = $db->createCommand('SELECT [[id]], [[width]], [[height]] FROM {{%ad_space}} WHERE [[tenant_id]] = :tenantId AND [[status]] = :status AND [[enabled]] = :enabled AND [[alias]] = :alias')->bindValues([
+                ':tenantId' => self::getConstantValue('TENANT_ID'),
+                ':status' => self::BOOLEAN_TRUE,
+                ':enabled' => self::BOOLEAN_TRUE,
+                ':alias' => strtolower(trim($alias))
+            ])->queryOne();
+        if ($space === false) {
+            return null;
+        }
+        $ads = $db->createCommand('SELECT [[id]], [[type]], [[url]], [[file_path]], [[text]], [[end_datetime]], [[message]] FROM {{%ad}} WHERE [[space_id]] = :spaceId AND [[status]] = :status AND [[enabled]] = :enabled AND [[begin_datetime]] <= :now ORDER BY [[end_datetime]] DESC')->bindValues([
+                ':spaceId' => $space['id'],
+                ':status' => self::BOOLEAN_TRUE,
+                ':enabled' => self::BOOLEAN_TRUE,
+                ':now' => time()
+            ])->queryAll();
+        if (!$ads) {
+            return [];
+        } else {
+            $rows = [];
+            foreach ($ads as $ad) {
+
+                if ($ad['end_datetime'] < time()) {
+                    $output = $ad['message'];
+                } else {
+                    $db->createCommand('UPDATE {{%ad}} SET [[hits_count]] = [[hits_count]] + 1, [[views_count]] = [[views_count]] + 1 WHERE [[id]] = :id')->bindValue(':id', $ad['id'], PDO::PARAM_INT)->execute();
+                    switch ($ad['type']) {
+                        case static::TYPE_PICTURE:
+                            $output = '<img src="' . $ad['file_path'] . '" width="' . $space['width'] . '" height="' . $space['height'] . '">';
+                            break;
+                        case static::TYPE_TEXT:
+                            $output = $ad['text'];
+                            break;
+                        default:
+                            $output = null;
+                            break;
+                    }
+                    if (!empty($ad['url'])) {
+                        $output = '<a href="' . $ad['url'] . '" target="_balnk">' . $output . '</a>';
+                    }
+                }
+                $rows[] = $output;
+            }
+
+            return $rows;
+        }
     }
 
     public static function one($alias)
