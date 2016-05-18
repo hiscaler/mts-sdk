9d72a2692ece41da5d98cbefc9eb92767cd0be77
hiscaler <hiscaler@gmail.com>
2016-05-18 22:36:00 +0800

    添加单文章数据拉取类

---
+++ src/yadjet/mts/sdk/ArticleGetter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace yadjet\mts\sdk;
+
+use yii\db\Query;
+
+/**
+ * 单文章数据拉取
+ * 
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class ArticleGetter extends DataGetter
+{
+
+    public static function one($alias)
+    {
+        return (new Query())->select(['alias', 'title', 'keywords', 'tags', 'keywords', 'description', 'picture_path', 'content'])
+                ->from('{{%article}}')
+                ->where([
+                    'tenant_id' => self::getConstantValue('TENANT_ID'),
+                    'alias' => trim($alias),
+                    'enabled' => self::BOOLEAN_TRUE
+                ])
+                ->one();
+    }
+
+}
